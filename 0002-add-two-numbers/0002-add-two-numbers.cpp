e57270494898c33fb3256ef46f71ab358d4e41c0
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-01 18:12:56 +0530

    Time: 18 ms (74.26%), Space: 76 MB (57.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,35 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1=l1;
-        ListNode* temp2=l2;
+    ListNode* addTwoNumbers(ListNode* temp1, ListNode* temp2) {
         int carry=0;
         ListNode* ans=new ListNode();
         ListNode* head=ans;
         while(temp1!=NULL || temp2!=NULL){
+            int tempval;
             if(temp1==NULL){
-                int tempval=carry+temp2->val;
-                ListNode* ansTemp=new ListNode(tempval%10);
-                carry=tempval/10;
+                tempval=carry+temp2->val;
+                //ListNode* ansTemp=new ListNode(tempval%10);
+                //carry=tempval/10;
                 temp2=temp2->next;
-                head->next=ansTemp;
+                //head->next=ansTemp;
             }
             else if(temp2==NULL){
-                int tempval=carry+temp1->val;
-                ListNode* ansTemp=new ListNode(tempval%10);
-                carry=tempval/10;
+                tempval=carry+temp1->val;
+                //ListNode* ansTemp=new ListNode(tempval%10);
+                //carry=tempval/10;
                 temp1=temp1->next;
-                head->next=ansTemp;
+                //head->next=ansTemp;
             }
             else{
-                int tempval=carry+temp1->val+temp2->val;
-                ListNode* ansTemp=new ListNode(tempval%10);
-                carry=tempval/10;
-                head->next=ansTemp;
+                tempval=carry+temp1->val+temp2->val;
+                //ListNode* ansTemp=new ListNode(tempval%10);
+                //carry=tempval/10;
+                //head->next=ansTemp;
                 temp1=temp1->next;
                 temp2=temp2->next;
             }
+            head->next=new ListNode(tempval%10);
+            carry=tempval/10;
             head=head->next;
         }
         if(carry>0){

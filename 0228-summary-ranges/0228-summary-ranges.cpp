43e10b30ea0f54c1cf7d4342b0e99e55647256bb
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-03 00:48:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (58.87%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        unordered_set<long long int>temp;
         vector<string>ans;
         int n=nums.size();
         for(int i=0; i<n; i++){
-            temp.insert(nums[i]);
-        }
-        for(int i=0; i<n; i++){
-            if(temp.find((long long int)nums[i]+1)==temp.end()){
-                ans.push_back(to_string(nums[i]));
+            int start=nums[i];
+            while(i+1<n && (nums[i+1]==(nums[i]+1))){
+                i++;
+            }
+            int end=nums[i];
+            if(start!=end){
+                ans.push_back(to_string(start)+"->"+to_string(end));
             }
             else{
-                int start=nums[i];
-                while(temp.find((long long int)nums[i]+1)!=temp.end()){
-                    i++;
-                }
-                int end=nums[i];
-                ans.push_back(to_string(start)+"->"+to_string(end));
+                ans.push_back(to_string(start));
             }
         }
         return ans;

f7666a47c9fa46dc127d9cf68b1f7b34e1d1a503
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-05 23:25:48 +0530

    Time: 3 ms (96.29%), Space: 11.3 MB (73.31%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        string ans=words[0];
+        for(int i=1; i<words.size(); i++){
+            string temp;
+            for(int j=0; j<ans.size(); j++){
+                if(words[i].find(ans[j])<words[i].length()){
+                    words[i][words[i].find(ans[j])]='0';
+                    temp.push_back(ans[j]);
+                }
+            }
+            //cout<<words[i]<<" "<<temp<<endl;
+            ans=temp;
+        }
+        vector<string>val;
+        for(int i=0; i<ans.size(); i++){
+            string temp(1, ans[i]);
+            val.push_back(temp);
+        }
+        return val;
+    }
+};

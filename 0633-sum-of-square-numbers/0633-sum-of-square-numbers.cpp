fc0cf1eb618a4ec55880469e631624bd3855c19f
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-18 03:35:20 +0530

    Time: 7 ms (23.25%), Space: 7.2 MB (18.55%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        if(c==0){
-            return true;
-        }
-        for(int i=1; i<=sqrt(c); i++){
+        for(int i=0; i<=sqrt(c); i++){
             int temp=sqrt(c-i*i);
             if(i*i+temp*temp==c){
                 return true;

d6f9adb620629cd80bc4787d4bb8e2f862358464
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-23 02:48:54 +0530

    Time: 0 ms (100%), Space: 7.1 MB (96.23%) - LeetHub

---
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int temp=0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                temp++;
+                if(temp==k){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};

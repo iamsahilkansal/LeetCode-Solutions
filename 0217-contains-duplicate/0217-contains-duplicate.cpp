2f384bf8b2789ff8bf9c1be7f08adbcca46e4fa9
Sahil Kansal <kansalsahil51@gmail.com>
2024-06-28 01:29:41 +0530

    Time: 83 ms (84.9%), Space: 73.3 MB (26.04%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> temp;
+        for(int i=0; i<nums.size(); i++){
+            if(temp.find(nums[i])!=temp.end()){
+                return true;
+            }
+            temp.insert(nums[i]);
+        }
+        return false;
+    }
+};

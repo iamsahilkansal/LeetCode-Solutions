66c049dc931924dfc440c4a77dd92728165cc109
Sahil Kansal <kansalsahil51@gmail.com>
2024-07-11 18:26:55 +0530

    Time: 2 ms (53.88%), Space: 7.5 MB (36.73%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int filled=numBottles, empty=0;
+        while(filled>0){
+            empty=empty+filled;
+            filled=0;
+            filled=empty/numExchange;
+            empty=empty%numExchange;
+            numBottles+=filled;
+        }
+        return numBottles;
+    }
+};

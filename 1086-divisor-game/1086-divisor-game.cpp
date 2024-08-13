f32bd564c30af715c7a58a996aa0728b605eba26
Sahil Kansal <kansalsahil51@gmail.com>
2024-08-13 16:26:36 +0530

    Time: 0 ms (100%), Space: 7.2 MB (63.67%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    int solve(int &n){
-        for(int x=1; x<n; x++){
-            if(n%x==0){
-                n=n-x;
-                return x;
-            }
-        }
-        return -1;
-    }
+    
     bool divisorGame(int n) {
-        while(n>0){
-            int x=solve(n);
-            if(x==-1){
-                return false;
-            }
-            int y=solve(n);
-            if(y==-1){
-                return true;
-            }
+        if(n%2==0){
+            return true;
         }
         return false;
     }

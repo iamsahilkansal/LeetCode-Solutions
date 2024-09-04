daf191e777a8b7c7417ddf2782ad7d25b8b403bf
Sahil Kansal <kansalsahil51@gmail.com>
2024-09-04 22:39:48 +0530

    Time: 2 ms (51.42%), Space: 7.2 MB (94.16%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int ans=0;
-        int i=0;
-        while(i<n){
-            ans=ans^(start+2*i);
-            i++;
+        int ans=start;
+        n--;
+        while(n){
+            ans=ans^(start+2*n);
+            n--;
         }
         return ans;
     }

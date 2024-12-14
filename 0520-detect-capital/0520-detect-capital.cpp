683d762dee895b2d37e68772363f2fd92589eb03
Sahil Kansal <kansalsahil51@gmail.com>
2024-12-14 19:13:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (14.49%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        bool flag1=true, flag2=true;
+        for(int i=1; i<word.size(); i++){
+            char x=word[i];
+            if(int(x)>90){
+                flag1=false;
+            }
+            if(int(x)<97){
+                flag2=false;
+            }
+        }
+        if(int(word[0])<=90 && flag1){
+            cout<<"1";
+            return true;
+        } 
+        if(int(word[0])>=97 && flag2){
+            cout<<"2";
+            return true;
+
+        } 
+        if(flag2) return true;
+        return false;
+    }
+};
